0847d7f988edc6fb8ca3632c975e53763aac831e
lattiia <india.lattimore@dillards.com>
2023-04-14 11:29:55 -0500

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SideComponent} from './side/side.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'side', component: SideComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideComponent } from './side/side.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideComponent
   ],
   imports: [
     BrowserModule,
